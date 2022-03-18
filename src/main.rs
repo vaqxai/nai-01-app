2bd7f814dc110c027c081192d78b72cfe8333481
umbranium <vakkendwarf@gmail.com>
2022-03-18 01:25:27 +0100

    add loading data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn load_data(filename: &str){
+
+    if let Ok(lines) = read_lines(filename){
+        for line in lines {
+            if let Ok(text) = line {
+                let columns: Vec<&str> = text.split(",").collect();
+                println!("{:?}", columns );
+            }
+        }
+    }
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    load_data("../iris/iris/test.txt");
 }
