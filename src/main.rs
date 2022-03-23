af25ee02b9af2a9ea6fa22690f35f3f23831540d
umbranium <vakkendwarf@gmail.com>
2022-03-23 09:11:29 +0100

    add knn

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,30 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::fmt;
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::hash::Hasher;
 
+#[derive(PartialEq)]
 struct DataObject {
+    id: usize,
     classifier: String,
     data: Vec<f32>
+}
+
+impl Eq for DataObject {
+}
+
+impl Hash for DataObject {
+    fn hash<H: Hasher>(&self, state: &mut H){
+        self.id.hash(state);
+    }
 }
 
 impl std::fmt::Display for DataObject {
 
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Data-Object: CLASS [{}], DATA: [{:?}]", self.classifier, self.data)
+        write!(f, "Data-Object: ID: [{}] CLASS [{}], DATA: [{:?}]", self.id, self.classifier, self.data)
     }
 
 }
@@ -23,6 +37,7 @@ impl TryFrom<&Vec<&str>> for DataObject {
     fn try_from(source: &Vec<&str>) -> Result<Self, Self::Error> {
         // assume that string-value is always last
 
+        let id = 0;
         let classifier: String;
 
         match source.get(source.len() - 1) {
@@ -44,7 +59,7 @@ impl TryFrom<&Vec<&str>> for DataObject {
             }
         }
 
-        Ok( DataObject {classifier, data} )
+        Ok( DataObject {id, classifier, data} )
     }
 
 }
@@ -55,7 +70,9 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn load_data(filename: &str){
+fn load_data(filename: &str) -> Result<Vec<DataObject>, io::Error> {
+
+    let mut res = Vec::<DataObject>::new();
 
     if let Ok(lines) = read_lines(filename){
         let mut i = 1;
@@ -63,7 +80,11 @@ fn load_data(filename: &str){
             if let Ok(text) = line {
                 let columns: Vec<&str> = text.split(",").collect();
                 match DataObject::try_from(&columns) {
-                    Ok(data_object) => println!("{}", data_object),
+                    Ok(mut data_object) => {
+                        data_object.id = i; // assign based on line number
+                        println!("{}", data_object);
+                        res.push(data_object);
+                    },
                     Err(errmsg) => println!("Could not parse columns on line {}: {} into DataObject, because: {}", i, text, errmsg)
                 }
             }
@@ -71,8 +92,131 @@ fn load_data(filename: &str){
         }
     }
 
+    Ok(res)
+
+}
+
+fn calc_dist(a: &DataObject, b: &DataObject) -> Result<f32, &'static str> {
+
+    if a.data.len() != b.data.len(){
+        return Err("Objects need to have the same dimensions!"); // err
+    }
+
+    let dim = a.data.len();
+    let mut sum= 0.0;
+
+    for i in 0..dim {
+        sum += f32::powf(a.data[i] - b.data[i], 2.0);
+    }
+
+    Ok(f32::sqrt(sum))
+
+}
+
+fn build_dist_table<'a>(a: &DataObject, data: &'a Vec<DataObject>) -> HashMap<&'a DataObject, f32> {
+
+    let mut res = HashMap::<&DataObject, f32>::new();
+
+    for b in data.iter() {
+        let x = match calc_dist(a,b) {
+            Ok(x) => x,
+            Err(msg) => panic!("Failure building distance table: {}", msg)
+        };
+        res.insert(b, x);
+    }
+
+    res
+
+}
+
+fn sort_by_dist<'a>(disttable: &mut HashMap<&'a DataObject, f32>, k: usize) -> Vec<&'a DataObject> {
+
+    let mut res = Vec::new();
+
+    let mut min: f32 = -1.0;
+    let mut minobj: Option<&DataObject> = None;
+
+    while res.len() < k {
+    // find min
+        for obj in disttable.iter() {
+
+            if res.len() == k { return res }
+
+            if min == -1.0 || min > *obj.1{
+                min = *obj.1;
+                minobj = Some(*obj.0);
+            }
+
+        }
+
+        if min == -1.0 {
+            panic!("Not enough neighbors to find nearest 3");
+        }
+
+        match minobj {
+            Some(x) => {
+                res.push(x);
+                disttable.remove(x);
+            },
+            None => panic!("Not enough neighbors to find nearest 3")
+        }
+
+    }
+
+    res
+
+}
+
+fn assign_knn_class(a: &mut DataObject, data: &Vec<DataObject>, k: usize) {
+
+    let dist_table = sort_by_dist(&mut build_dist_table(a, data), k);
+    let mut fr_tab: HashMap<String, usize> = HashMap::new();
+
+    for obj in dist_table {
+
+        let option_res = fr_tab.get(&obj.classifier);
+
+        let v = match option_res {
+            Some(&k) => k,
+            None => 1
+        };
+
+        let class = &obj.classifier;
+        fr_tab.insert(class.to_string(), v);
+    }
+
+    let mut max = 0;
+    let mut max_class: Option<String> = None;
+
+    for entry in fr_tab {
+        if entry.1 > max{
+            max_class = Some(entry.0);
+            max = entry.1;
+        }
+    }
+
+    match max_class {
+        Some(s) => a.classifier = s,
+        None => return
+    }
+
 }
 
 fn main() {
-    load_data("../iris/iris/train.txt");
+
+    let data = match load_data("../iris/iris/train.txt") {
+        Ok(data) => data,
+        Err(e) => panic!("Error! {}", e)
+    };
+
+    let mut example_obj = DataObject {
+        id: 0,
+        classifier: "?".to_string(),
+        data: vec![4.7,3.2,1.6,0.2],
+    };
+
+    assign_knn_class(&mut example_obj, &data, 3);
+
+    println!("The assigned class of example_obj is: {}, should be: {}", example_obj.classifier, "Iris-setosa");
+
 }
